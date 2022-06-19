4ecab647538700295c3194d108475749ef020f75
DandanIsmail42 <dandanismail42@gmail.com>
2022-06-19 06:14:10 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import { tampil } from "./module.js";
+function tes() {
+    let first = tampil();
+    document.getElementById('wper').innerHTML = first;
+}
+tes();
---
+++ module.js
@@ -0,0 +1,36 @@
+
+function tampil() {
+  new gridjs.Grid({
+    columns: ["Nama Barang", "Jenis Barang", "Kode Barang"],
+    data: [
+      ["Aqua", "Minuman", "001"],
+      ["Bubur kemasan", "Makanan", "002"],
+      ["Sosis", "Makanan", "003"],
+      ["Fanta", "Minuman", "004"],
+      ["Sarden", "Makanan", "005"],
+      ["Mie instan", "Makanan", "006"]
+      
+    ]
+  }).render(document.getElementById("wrapper"));
+
+
+function helloWorld () {
+  const wrapperRef = useRef(null);
+
+  const grid = new Grid({
+    columns: ['Nama barang', 'Jenis barang', 'Kode Barang'],
+    data: [
+      ['Aqua', 'Minuman', '001'],
+      ['Bubur kemasan', 'Makanan', '002']
+    ]
+  });
+  
+  useEffect(() => {
+    grid.render(wrapperRef.current);
+  });
+  
+  return ref={wrapperRef};
+}
+}
+
+export { tampil };
